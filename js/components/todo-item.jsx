364ffe805e9c5c04acd2e62c3c0c952f203fae06
Liu Yang <zation1@gmail.com>
2015-01-18 22:04:44 +0800

    move all DOM event into todo-item.jsx

--- js/components/todo-item.jsx
+++ js/components/todo-item.jsx
@@ -7,8 +7,82 @@ var app = app || {};
 
 		getInitialState: function() {
 			return {
-				completed: this.props.todo.get('completed')
+				todo: this.props.todo.toJSON(),
+				isEditing: false
 			};
+		},
+
+		setStateOfTodo: function() {
+			this.setState({
+				todo: this.props.todo.toJSON()
+			});
+		},
+
+		componentDidMount: function() {
+			this.props.todo.on('change',this.setStateOfTodo, this);
+		},
+
+		componentWillUnmount: function() {
+			this.props.todo.off('change', this.setStateOfTodo, this);
+		},
+
+		// Switch this view into `"editing"` mode, displaying the input field.
+		edit: function () {
+			this.setState({
+				isEditing: true
+			});
+			// TODO: should find a good way to focus input after it's visible
+			var editInput = this.refs.editInput;
+			window.setTimeout(function() {
+				editInput.getDOMNode().focus();
+			}, 1);
+		},
+
+		// Close the `"editing"` mode, saving changes to the todo.
+		close: function () {
+			var value = this.refs.editInput.getDOMNode().value;
+			var trimmedValue = value.trim();
+
+			// We don't want to handle blur events from an item that is no
+			// longer being edited. Relying on the state here has the
+			// benefit of us not having to maintain state in the DOM and the
+			// JavaScript logic.
+			if (!this.state.isEditing) {
+				return;
+			}
+
+			if (trimmedValue) {
+				this.props.todo.save({ title: trimmedValue });
+			} else {
+				this.clear();
+			}
+
+			this.setState({
+				isEditing: false
+			});
+		},
+
+		clear: function () {
+			this.props.todo.destroy();
+		},
+
+		// If you hit `enter`, we're through editing the item.
+		updateOnEnter: function (e) {
+			if (e.which === ENTER_KEY) {
+				this.close();
+			}
+		},
+
+		// If you're pressing `escape` we revert your change by simply leaving
+		// the `editing` state.
+		revertOnEscape: function (e) {
+			if (e.which === ESC_KEY) {
+				this.setState({
+					isEditing: false
+				});
+				// Also reset the hidden input back to the original value.
+				this.refs.editInput.getDOMNode().value = this.state.todo.title;
+			}
 		},
 
 		// Toggle the `"completed"` state of the model.
@@ -18,25 +92,24 @@ var app = app || {};
 			});
 		},
 
-		// Switch this view into `"editing"` mode, displaying the input field.
-		edit: function () {
-			this.refs.editInput.getDOMNode().focus();
-		},
-
 		render: function() {
-			var todoData = this.props.todo.toJSON();
 			return (
-				<div>
+				<li className={React.addons.classSet({'editing': this.state.isEditing})}>
 					<div className="view">
 						<input className="toggle"
 							type="checkbox"
-							checked={this.state.completed}
+							checked={this.state.todo.completed}
 							onChange={this.toggleCompleted}/>
-						<label onDoubleClick={this.edit}>{todoData.title}</label>
-						<button className="destroy"></button>
+						<label onDoubleClick={this.edit}>{this.state.todo.title}</label>
+						<button onClick={this.clear} className="destroy"></button>
 					</div>
-					<input ref="editInput" className="edit" defaultValue={todoData.title} />
-				</div>
+					<input ref="editInput"
+						onBlur={this.close}
+						className="edit"
+						onKeyPress={this.updateOnEnter}
+						onKeyDown={this.revertOnEscape}
+						defaultValue={this.state.todo.title} />
+				</li>
 			);
 		}
 	});
--- js/views/todo-view.js
+++ js/views/todo-view.js
@@ -12,15 +12,6 @@ var app = app || {};
 		//... is a list tag.
 		tagName:  'li',
 
-		// The DOM events specific to an item.
-		events: {
-			'dblclick label': 'edit',
-			'click .destroy': 'clear',
-			'keypress .edit': 'updateOnEnter',
-			'keydown .edit': 'revertOnEscape',
-			'blur .edit': 'close'
-		},
-
 		// The TodoView listens for changes to its model, re-rendering. Since
 		// there's a one-to-one correspondence between a **Todo** and a
 		// **TodoView** in this app, we set a direct reference on the model for
@@ -49,7 +40,6 @@ var app = app || {};
 			}), this.$el[0]);
 			this.$el.toggleClass('completed', this.model.get('completed'));
 			this.toggleVisible();
-			this.$input = this.$('.edit');
 			return this;
 		},
 
@@ -61,64 +51,6 @@ var app = app || {};
 			return this.model.get('completed') ?
 				app.TodoFilter === 'active' :
 				app.TodoFilter === 'completed';
-		},
-
-		// Switch this view into `"editing"` mode, displaying the input field.
-		edit: function () {
-			this.$el.addClass('editing');
-		},
-
-		// Close the `"editing"` mode, saving changes to the todo.
-		close: function () {
-			var value = this.$input.val();
-			var trimmedValue = value.trim();
-
-			// We don't want to handle blur events from an item that is no
-			// longer being edited. Relying on the CSS class here has the
-			// benefit of us not having to maintain state in the DOM and the
-			// JavaScript logic.
-			if (!this.$el.hasClass('editing')) {
-				return;
-			}
-
-			if (trimmedValue) {
-				this.model.save({ title: trimmedValue });
-
-				if (value !== trimmedValue) {
-					// Model values changes consisting of whitespaces only are
-					// not causing change to be triggered Therefore we've to
-					// compare untrimmed version with a trimmed one to check
-					// whether anything changed
-					// And if yes, we've to trigger change event ourselves
-					this.model.trigger('change');
-				}
-			} else {
-				this.clear();
-			}
-
-			this.$el.removeClass('editing');
-		},
-
-		// If you hit `enter`, we're through editing the item.
-		updateOnEnter: function (e) {
-			if (e.which === ENTER_KEY) {
-				this.close();
-			}
-		},
-
-		// If you're pressing `escape` we revert your change by simply leaving
-		// the `editing` state.
-		revertOnEscape: function (e) {
-			if (e.which === ESC_KEY) {
-				this.$el.removeClass('editing');
-				// Also reset the hidden input back to the original value.
-				this.$input.val(this.model.get('title'));
-			}
-		},
-
-		// Remove the item, destroy the model from *localStorage* and delete its view.
-		clear: function () {
-			this.model.destroy();
 		}
 	});
 })(jQuery);
